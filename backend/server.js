b067887bef51220bb31dc8ddaa22b785da10631f
vibhor2310 <95543808+vibhor2310@users.noreply.github.com>
2023-07-14 23:15:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chit-chat-app-vh41.onrender.com/",
+    origin: "http://localhost:3000",
     // credentials: true,
   },
 });
